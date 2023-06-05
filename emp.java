db55ea4e6d308e9b78f7a95311f775c05fb24420
savithareddy26 <savithareddy26@gmail.com>
2023-06-05 19:02:51 -0400

    employee file

---
+++ emp.java
@@ -0,0 +1,12 @@
+package employeee;
+
+public class emp {
+	
+		public String empname;
+		public int empid;
+		public double salary;
+		public static void main(String[] args) {
+			System.out.println("Hello this is an employee portal");
+		}
+}
+
